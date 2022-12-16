56ad46254edf925ba5916c85b02f405a92762ad2
Divyesh Shrestha <rashudshrestha12@gmail.com>
2022-12-16 21:29:16 +1100

    my-second-commit

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Home from '../Pages/Home'
 
+import {Route, Routes} from 'react-router-dom'
+import Cuisine from '../Pages/Cuisine'
+
 function Page() {
   return (
-    <div>
-        <Home />
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/cuisine/:type" element ={<Cuisine />} />
+    </Routes>
+   
   )
 }
 
