7ce43ab11447ac7f076eee281593b674e3146cdc
Divyesh Shrestha <rashudshrestha12@gmail.com>
2022-12-18 13:34:52 +1100

    my-third-push

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import Pages from './Pages/Page'
 import Category from './components/Category'
 import { BrowserRouter } from 'react-router-dom';
+import Search from './components/Search'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
--- src/Pages/Cuisine.js
+++ src/Pages/Cuisine.js
@@ -6,9 +6,10 @@ import { useParams } from 'react-router-dom'
 function Cuisine() {
     const [cuisine, setCuisine] = useState([])
     let params = useParams()
+    console.log(params)
 
     const getCuisine= async(name) =>{
-        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine={name}`)
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=9&cuisine=${name}`);
         const recipes= await data.json()
         setCuisine(recipes.results)
     }
@@ -32,8 +33,8 @@ function Cuisine() {
 }
 const Grid = styled.div`
 display:grid;
-grid-template-columns:repeat(auto-fit, minmax(20rem, 1fr));
-grid-gap: 3rem;
+grid-template-columns:repeat(auto-fit, minmax(10rem, 1fr));
+grid-gap: 5rem;
 `
 const Card = styled.div`
 img{
